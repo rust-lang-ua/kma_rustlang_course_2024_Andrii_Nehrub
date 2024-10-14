24e373c33ae34485dd830d15fda84e767c4001e2
Andrii Negrub <zurg0505@gmail.com>
2024-10-15 01:10:41 +0300

    Feat: done quiz4 macros and clippy

--- rustlings/exercises/quiz4.rs
+++ rustlings/exercises/quiz4.rs
@@ -5,7 +5,14 @@
 
 // Write a macro that passes the quiz! No hints this time, you can do it!
 
-// I AM NOT DONE
+macro_rules! my_macro {
+    () => {
+        "Hello!".to_string()
+    };
+    ($val:expr) => {
+        "Hello ".to_string() + $val
+    };
+}
 
 #[cfg(test)]
 mod tests {
